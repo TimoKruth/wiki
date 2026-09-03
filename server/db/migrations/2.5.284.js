affed1dba0e2bbdbe0e054b50834d46ef82e6854
Timo Kruth <timo_kruth@gmx.de>
2026-09-03 20:54:21 +0200

    fix(db): make ToC migration compatible with MySQL

--- server/db/migrations/2.5.284.js
+++ server/db/migrations/2.5.284.js
@@ -1,12 +1,15 @@
 exports.up = async knex => {
   await knex.schema
     .alterTable('pages', table => {
-      table.json('tocOptions').notNullable().defaultTo(JSON.stringify({
-        min: 1,
-        max: 2,
-        useDefault: true
-      }))
+      table.json('tocOptions').nullable()
     })
+  await knex('pages').whereNull('tocOptions').update({
+    tocOptions: JSON.stringify({
+      min: 1,
+      max: 2,
+      useDefault: true
+    })
+  })
 }
 
 exports.down = knex => { }
